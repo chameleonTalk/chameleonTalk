89faba6808642b341d3a34ffb2ef30c49194e436
Jeanne Pascua <jpascua93@gmail.com>
2016-01-20 15:58:04 -0800

    Reorganized some lines

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('./client'));
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 require('./api/routes')(app)
 
@@ -11,8 +12,6 @@ app.get('*', function (req, res) {
 	res.sendFile('/client/views/index.html', { root: __dirname });
 });
 
-var io = require('socket.io')(server);
-
-app.listen(9091, function () {
-	console.log('Server is running on 9091.')
+app.listen(8080, function () {
+	console.log('Server is running on 8080.');
 });
